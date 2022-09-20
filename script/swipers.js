8259f70c6a3c4804873ed180e3edc1b82f9fc6a4
Ark21bit <webleif@vk.com>
2022-09-20 11:25:04 +0300

    почти готово

--- script/swipers.js
+++ script/swipers.js
@@ -1,16 +1,29 @@
       const swiper = new Swiper(".mySwiper", {
         loop: true,
-        spaceBetween: 17,
-        slidesPerView: 5,
+        spaceBetween: 13,
+        slidesPerView: 3,
         freeMode: true,
-        watchSlidesProgress: true,
+        watchSlidesProgress: true,        
         navigation: {
             nextEl: ".swiper-button-next"            
           },
+        breakpoints: {            
+            768: {
+              slidesPerView: 5,   
+              spaceBetween: 17,           
+            },  
+
+            449: {
+              slidesPerView: 4,   
+
+            },  
+
+        }        
       });
       
       const swiper2 = new Swiper(".mySwiper2", {
         loop: true,
+        height:500,
         spaceBetween: 10, 
         thumbs: {
           swiper: swiper,
