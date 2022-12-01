922877def55ffb4b1f06728190e2bfe58128f25b
Ark21bit <webleif@vk.com>
2022-12-01 16:13:53 +0300

    почти готово

--- script/swipers.js
+++ script/swipers.js
@@ -29,4 +29,32 @@
           swiper: swiper,
         },
       });
+
+      const swiper3 = new Swiper('.mySwiper3', { 
+        spaceBetween:24,
+        slidesPerView: 'auto',       
+        navigation: {
+          nextEl: '.choose .swiper-button-next',
+          prevEl: '.choose .swiper-button-prev',
+        },
+      
+      });
+
+      const swiper4 = new Swiper('.mySwiper4', {         
+        slidesPerView: 1,       
+        pagination: {
+          el: '.swiper-pagination',
+          clickable:true,          
+          renderBullet: function (index, className) {
+            let arr = ["2003", "2008", "2009", "2012", "2016", "2018", "2020" , "2022"] ;
+            return '<span class="' + className + '">' + (arr[index]) + "</span>";
+          },
+        },
+        navigation: {
+          nextEl: '.history .swiper-button-next',
+          prevEl: '.history .swiper-button-prev',
+        },
+      
+      });
+
     
