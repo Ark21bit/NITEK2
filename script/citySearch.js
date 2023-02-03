aa6225718c555d6825b87c3dc3f071e4b149304e
Ark21bit <90682332+Ark21bit@users.noreply.github.com>
2023-02-03 20:03:58 +0000

    обновленное меню

--- script/citySearch.js
+++ script/citySearch.js
@@ -10,10 +10,17 @@ $('#search_clear').click(function(){
 
 function filterCitys() {
     $(".citys-item" ).each(function( ) {  
-        if ($(this).data("city").toLowerCase().indexOf($(".search_input").val().toLowerCase()) ) {       
-            $(this).fadeOut();  
+        if ($(this).data("city").toLowerCase().includes($(".search_input").val().toLowerCase()) ) {       
+            $(this).show();                
+            if ($(this).parents('.citys-dropdown').length > 0) {
+                $(this).parents('.citys-dropdown').show(); 
+            } 
         }else{        
-            $(this).fadeIn();     
+            $(this).hide();             
         }        
     });
 }
+
+$('.citys-dropdown-toggle').click(function(){
+    $(this).parents('.citys-dropdown')[0].classList.toggle('active');
+});
